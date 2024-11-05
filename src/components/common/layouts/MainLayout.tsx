1e970f542b7016f04c5011a60c63c32b5bf40b1b
Hyoeunkh <lhe4528@knu.ac.kr>
2024-11-06 01:02:21 +0900

    feat: 에러 컴포넌트 적용

--- src/components/common/layouts/MainLayout.tsx
+++ src/components/common/layouts/MainLayout.tsx
@@ -2,18 +2,27 @@ import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import styled from 'styled-components';
 
+import { ErrorBoundary } from 'react-error-boundary';
+import { QueryErrorResetBoundary } from '@tanstack/react-query';
 import Footer from '@/components/common/layouts/Footer';
 import Header from '@/components/common/layouts/Header';
 import Loading from '@/components/common/layouts/Loading';
+import Error from '@/components/common/layouts/Error';
 
 export default function MainLayout() {
   return (
     <Wrapper>
       <Header />
       <InnerWrapper>
-        <Suspense fallback={<Loading size={50} />}>
-          <Outlet />
-        </Suspense>
+        <QueryErrorResetBoundary>
+          {({ reset }) => (
+            <ErrorBoundary FallbackComponent={Error} onReset={reset}>
+              <Suspense fallback={<Loading size={50} />}>
+                <Outlet />
+              </Suspense>
+            </ErrorBoundary>
+          )}
+        </QueryErrorResetBoundary>
       </InnerWrapper>
       <Footer />
     </Wrapper>
@@ -22,12 +31,16 @@ export default function MainLayout() {
 
 const Wrapper = styled.div`
   width: 960px;
+  height: 100vh;
   margin: 0 auto;
   position: relative;
+  display: flex;
+  flex-direction: column;
 `;
 
 const InnerWrapper = styled.div`
-  width: 100%;
-  margin-top: 26px;
+  display: flex;
   flex-grow: 1;
+  flex-direction: column;
+  width: 100%;
 `;
