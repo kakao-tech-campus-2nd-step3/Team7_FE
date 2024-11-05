0967de57b6492301f5847712960666c3cc620692
Hyoeunkh <lhe4528@knu.ac.kr>
2024-11-06 01:01:45 +0900

    fix: 로그인 여부 확인 수정

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -85,7 +85,7 @@ export default function MainPage() {
         SubText=" 가 방문한 장소를 찾아볼까요?"
         items={influencersData.influencers}
       />
-      {authInfo ? (
+      {authInfo.accessToken ? (
         <>
           <BaseLayout
             type="spot"
