4d4ef84040afa50df9d04b990538e99a28b327cd
Hyoeunkh <lhe4528@knu.ac.kr>
2024-11-07 20:44:59 +0900

    style: api호출 경로 오타 수정

--- src/api/hooks/usePostPlaceLike.ts
+++ src/api/hooks/usePostPlaceLike.ts
@@ -3,7 +3,7 @@ import { useMutation } from '@tanstack/react-query';
 import { fetchInstance } from '../instance';
 import { RequestPlaceLike } from '@/types';
 
-export const postPlaceLikePath = () => `/Places/likes`;
+export const postPlaceLikePath = () => `/places/likes`;
 const postPlaceLike = async ({ placeId, likes }: RequestPlaceLike) => {
   const response = await fetchInstance.post(postPlaceLikePath(), {
     placeId,
