58bb568a1d6ae97f0dd794e468686ff367a63a72
Hyoeunkh <lhe4528@knu.ac.kr>
2024-11-10 00:01:33 +0900

    feat: 메인, 세부 페이지 스켈레톤 컴포넌트 추가

---
+++ src/components/ui/Skeleton.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+type SkeletonProps = {
+  width?: string;
+  height?: string;
+  borderRadius?: string;
+} & React.HtmlHTMLAttributes<HTMLDivElement>;
+
+export default function Skeleton({ width = '100%', height, borderRadius = '4px', ...props }: SkeletonProps) {
+  return <Wrapper width={width} height={height} borderRadius={borderRadius} {...props} />;
+}
+
+const pulse = keyframes`
+  0%, 100% {
+    opacity: 0.4;
+  }
+  50% {
+    opacity: 0.1;
+  }
+`;
+const Wrapper = styled.div<Pick<SkeletonProps, 'width' | 'height' | 'borderRadius'>>`
+  display: inline-block;
+  background-color: #828282;
+  width: ${({ width }) => `${width}`};
+  height: ${({ height }) => `${height}`};
+  border-radius: ${({ borderRadius }) => `${borderRadius}`};
+  animation: ${pulse} 2s ease-in-out infinite;
+`;
