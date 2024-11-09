458cdd2298c1f288eee89b456b3a16b2216785f1
userjmmm <141299582+userjmmm@users.noreply.github.com>
2024-11-09 21:34:35 +0900

    [오류] s3 배포시 누락된 링크를 추가하고, vite에 맞는 경로를 설정했어요 (#81)

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 
 export default defineConfig({
+  base: './',
   plugins: [react(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+      },
+    },
+  },
 });
