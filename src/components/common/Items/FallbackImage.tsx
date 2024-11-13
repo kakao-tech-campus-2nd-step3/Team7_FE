efa76d57ba76ee270d5e984bb817abf99f56a843
userjmmm <a@as-MacBook-Pro.local>
2024-11-13 20:06:48 +0900

    feat: fallback image를 렌더링하는 컴포넌트 모든 컴포넌트에 사용

--- src/components/common/Items/FallbackImage.tsx
+++ src/components/common/Items/FallbackImage.tsx
@@ -28,5 +28,6 @@ export default function FallbackImage({ src, fallbackSrc = BasicImage, alt = '',
 const StyledImage = styled.img`
   width: 100%;
   height: 100%;
+  object-fit: cover;
   border-radius: inherit;
 `;
