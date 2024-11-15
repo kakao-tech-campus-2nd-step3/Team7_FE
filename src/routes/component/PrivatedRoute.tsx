e7299eb41b687850fe1f332ea6159cc41c5f01e8
userjmmm <a@as-MacBook-Pro.local>
2024-11-15 17:13:48 +0900

    style: lint 오류 수정

--- src/routes/component/PrivatedRoute.tsx
+++ src/routes/component/PrivatedRoute.tsx
@@ -1,6 +1,5 @@
 import { ReactElement, useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { AxiosError } from 'axios';
 import useAuth from '@/hooks/useAuth';
 import LoginModal from '@/components/common/modals/LoginModal';
 import { useGetUserInfo, isAuthorizationError } from '@/api/hooks/useGetUserInfo';
