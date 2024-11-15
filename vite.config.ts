84b9ca03b9db7416142035392a18c06ed6181464
userjmmm <a@as-MacBook-Pro.local>
2024-11-15 13:43:10 +0900

    style: vite.config.ts에 사용하는 경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react(), tsconfigPaths()],
   build: {
     rollupOptions: {
