90c62e17552a7a9f3d324b1a0b020d554c9decf1
Hyoeunkh <lhe4528@knu.ac.kr>
2024-11-15 13:06:58 +0900

    fix: 동적 페이지 새로고침 시 흰창 뜨는 에러 해결

--- src/main.tsx
+++ src/main.tsx
@@ -18,7 +18,7 @@ startApp().then(() => {
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <App />
         </BrowserRouter>
       </QueryClientProvider>
